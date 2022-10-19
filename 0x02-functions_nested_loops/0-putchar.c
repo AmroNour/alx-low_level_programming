439da3e5651ad34a3876090b8ccf7e3db2779e9e
AmroNour <3mronour2003@gmail.com>
2022-10-18 17:59:05 -0700

    Clang2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * main - program that prints Holberton.
+ * main - program that prints _putchar.
  * Return: int
  */
 
 int main(void)
 {
-	int n = 0;
-	char str_hbt[9] = "_putchar";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	while (n < 9)
-	{
-		_putchar(str_hbt[n]);
-		n += 1;
-	}
-	_putchar('\n');
 	return (0);
 }
